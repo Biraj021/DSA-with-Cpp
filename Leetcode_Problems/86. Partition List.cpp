18c26fb9312dabe2cd8966f560203fe38ae45fc7
Biraj Acherjee <birajacherjee7@gmail.com>
2026-01-16 21:33:49 +0530

    Update 86. Partition List.cpp

---
+++ Leetcode_Problems/86. Partition List.cpp
@@ -0,0 +1,59 @@
+// Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+// You should preserve the original relative order of the nodes in each of the two partitions.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,4,3,2,5,2], x = 3
+// Output: [1,2,2,4,3,5]
+// Example 2:
+
+// Input: head = [2,1], x = 2
+// Output: [1,2]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 200].
+// -100 <= Node.val <= 100
+// -200 <= x <= 200
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* low=new ListNode(100);
+        ListNode* high=new ListNode(1);
+        ListNode* templo=low;
+        ListNode* temphi=high;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(temp->val<x){
+                templo->next=temp;
+                temp=temp->next;
+                templo=templo->next;
+            }
+            else{
+                temphi->next=temp;
+                temp=temp->next;
+                temphi=temphi->next;
+            }
+        }
+
+    
+        templo->next=high->next;
+        temphi->next=NULL;
+        return low->next;
+    }
+};
