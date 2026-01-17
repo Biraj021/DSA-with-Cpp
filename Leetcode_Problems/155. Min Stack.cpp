08d77a227d7b1e55d23e8d67378215bd5bf5a9e6
Biraj Acherjee <birajacherjee7@gmail.com>
2026-01-17 21:43:18 +0530

    Update 155. Min Stack.cpp

---
+++ Leetcode_Problems/155. Min Stack.cpp
@@ -0,0 +1,65 @@
+// /Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// Implement the MinStack class:
+
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+// Example 1:
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+ 
+
+// Constraints:
+
+// -231 <= val <= 231 - 1
+// Methods pop, top and getMin operations will always be called on non-empty stacks.
+// At most 3 * 104 calls will be made to push, pop, top, and getMin.
+class MinStack {
+public:
+vector<int> v;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        v.push_back(val);
+    }
+    
+    void pop() {
+       v.pop_back();
+    }
+    
+    int top() {
+       return v[v.size()-1];
+    }
+    
+    int getMin() {
+        int m=v[0];
+        for(int i=1;i<v.size();i++){
+            m=min(m,v[i]);
+        }
+        return m;
+    }
+};
