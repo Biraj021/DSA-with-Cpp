c4248c278aba5b4dc6d0dd74f534415bd269d5a8
Biraj Acherjee <birajacherjee7@gmail.com>
2026-01-19 21:33:27 +0530

    Add function to merge two sorted linked lists
    Implement mergeTwoLists function to merge two sorted linked lists.

---
+++ Leetcode_Problems/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,61 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+ 
+
+// Example 1:
+
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+     ListNode* c=new ListNode(-1);
+     ListNode* temp=c;
+     while(a!=NULL&&b!=NULL){
+        if(a->val<=b->val){
+            temp->next=a;
+            a=a->next;
+            temp=temp->next;
+        }
+        else{
+            temp->next=b;
+            b=b->next;
+            temp=temp->next;
+        }
+
+     }   
+     if(a==NULL) temp->next=b;
+     else temp->next=a;
+     return c->next;
+    }
+};
