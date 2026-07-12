3f4c8b5b0a25e1cb2157a6abc91a2d501d257650
Biraj Acherjee <birajacherjee7@gmail.com>
2026-07-12 22:58:57 +0530

    Refactor ticket buying logic in C++ solution

--- Leetcode_Problems/2073. Time Needed to Buy Tickets.cpp
+++ Leetcode_Problems/2073. Time Needed to Buy Tickets.cpp
@@ -47,7 +47,6 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int time = 0;
-
         for (int i = 0; i < tickets.size(); i++) {
             if (i < k) {
                 time += min(tickets[i], tickets[k]);
@@ -55,11 +54,10 @@ public:
             else if (i == k) {
                 time += tickets[k];
             }
-            else { // i > k
+            else { 
                 time += min(tickets[i], tickets[k] - 1);
             }
         }
-
         return time;
     }
 };
