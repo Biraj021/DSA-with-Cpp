cdb878ede9fb6a3f6f5778b7e7d8023ba9fc11a0
Biraj Acherjee <birajacherjee7@gmail.com>
2026-07-12 22:17:24 +0530

    Update 66. Plus One.cpp

--- Leetcode_Problems/66. Plus One.cpp
+++ Leetcode_Problems/66. Plus One.cpp
@@ -36,8 +36,6 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-
-        // start from last digit
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
@@ -45,8 +43,6 @@ public:
             }
             digits[i] = 0;
         }
-
-        // if all digits were 9
         digits.insert(digits.begin(), 1);
         return digits;
     }
