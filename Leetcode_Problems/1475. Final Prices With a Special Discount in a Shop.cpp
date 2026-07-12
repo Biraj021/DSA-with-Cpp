0a921fe9d8d2fc55494d88318346da1369d1360c
Biraj Acherjee <birajacherjee7@gmail.com>
2026-07-12 22:51:03 +0530

    Update 1475. Final Prices With a Special Discount in a Shop.cpp

--- Leetcode_Problems/1475. Final Prices With a Special Discount in a Shop.cpp
+++ Leetcode_Problems/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -34,20 +34,16 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         vector<int> ans;
-
         for (int i = 0; i < prices.size(); i++) {
-            int finalPrice = prices[i];   // assume no discount
-
+            int finalPrice = prices[i];   
             for (int j = i + 1; j < prices.size(); j++) {
                 if (prices[j] <= prices[i]) {
                     finalPrice = prices[i] - prices[j];
-                    break;               // first valid discount
+                    break;               
                 }
             }
-
-            ans.push_back(finalPrice);   // push exactly once
+            ans.push_back(finalPrice);  
         }
-
         return ans;
     }
 };
